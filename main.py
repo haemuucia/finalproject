265147f394d7be1584325d5a78f7d96b7d1c795f
haemuucia <162682171+haemuucia@users.noreply.github.com>
2024-07-27 20:09:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -289,12 +289,14 @@ def fragment_possible_causes_container(temp, ph, tds, ec):
   }
 
   html_possible_causes = "".join([
-    f'<strong class="text-xl sm:text-2xl">{key}</strong>'
-    f'<ul class="list-disc mb-5 sm:ml-5">'
-    f'<li>'
-    f'{"".join([f"<span>{cause}</span>" for cause in causes])}' 
-    f'</li>'
-    f'</ul>'
+   html_possible_causes  = ""
+  for key, causes in possible_causes.items():
+    if causes:
+        html_possible_causes += f'<strong class="text-xl sm:text-2xl">{key}</strong>'
+        html_possible_causes += '<ul class="list-none sm:list-disc mb-5 sm:ml-5">'
+        for cause in causes:
+            html_possible_causes += f'<li class="text-sm sm:text-2xl">{cause}</li>'
+        html_possible_causes += '</ul>'
     for key, causes in possible_causes.items() if causes
   ])
   
@@ -316,13 +318,14 @@ def fragment_suggestions_container(temp, ph, tds, ec):
     "EC": ec_suggestion(ec)
   }
 
-  html_possible_suggestions = "".join([
-    f'<strong class="text-xl sm:text-2xl">{key}</strong>'
-    f'<ul class="list-disc mb-5 sm:ml-5">'
-    f'<li>'
-    f'{"".join([f"<span>{suggestion}</span>" for suggestion in suggestions])}'
-    f'</li>'
-    f'</ul>'
+  html_possible_suggestions = ""
+  for key, suggestions in possible_suggestions.items():
+    if suggestions:
+        html_possible_suggestions += f'<strong class="text-xl sm:text-2xl">{key}</strong>'
+        html_possible_suggestions += '<ul class="list-none sm:list-disc mb-5 sm:ml-5">'
+        for suggestion in suggestions:
+            html_possible_suggestions += f'<li class="text-sm sm:text-2xl">{suggestion}</li>'
+        html_possible_suggestions += '</ul>'
     for key, suggestions in possible_suggestions.items() if suggestions
   ])
  
