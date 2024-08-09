bdd86ea07feb633299523a971d9c54f0adce1fcd
haemuucia <162682171+haemuucia@users.noreply.github.com>
2024-08-09 16:56:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -316,11 +316,11 @@ def fragment_suggestions_container(temp, ph, tds, ec):
 
  html_possible_suggestions = "".join([
     f'''<strong class="text-xl sm:text-2xl">{key}</strong>
-    <ul class="list-disc mb-5 sm:ml-5>'''
+    <ul class="list-disc mb-5 sm:ml-5">'''
     + "".join([f'''<li><span class="text-sm sm:text-2xl">{suggestion}</span></li>''' for suggestion in suggestions])
-    + f'''</ul>
-    ''' for key, suggestions in possible_suggestions.items() if suggestions
- ])
+    + f'''</ul>'''
+    for key, suggestions in possible_suggestions.items() if suggestions
+])
  
   html_suggestion_container = f'''
     <div style="color: #1A3E7E;" class="w-full p-5 sm:p-10 flex flex-col bg-white rounded-xl shadow-black shadow-2xl">
