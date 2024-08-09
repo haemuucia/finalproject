9cd0297e133cc987ab76a956f8b027a96902c90e
haemuucia <162682171+haemuucia@users.noreply.github.com>
2024-08-09 21:17:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -293,10 +293,10 @@ def fragment_possible_causes_container(temp, ph, tds, ec):
   }
 
   html_possible_causes = "".join([
-    f'<strong class="text-xl sm:text-2xl">{key}</strong>'
-    f'<ul class="list-disc mb-5 sm:ml-5">'
-    f'{"".join([f"<li><span class=\"text-sm sm:text-2xl\">{cause}</span></li>" for cause in causes])}'
-    f'</ul>'
+    '<strong class="text-xl sm:text-2xl">{}</strong>'.format(key) + 
+    '<ul class="list-disc mb-5 sm:ml-5">' + 
+    ''.join(['<li><span class="text-sm sm:text-2xl">{}</span></li>'.format(cause) for cause in causes]) + 
+    '</ul>'
     for key, causes in possible_causes.items() if causes != [] and causes is not None
   ])
   
@@ -319,10 +319,10 @@ def fragment_suggestions_container(temp, ph, tds, ec):
   }
 
   html_possible_suggestions = "".join([
-    f'<strong class="text-xl sm:text-2xl">{key}</strong>'
-    f'<ul class="list-disc mb-5 sm:ml-5">'
-    f'{"".join([f"<li><span class=\"text-sm sm:text-2xl\">{suggestion}</span></li>" for suggestion in suggestions])}'
-    f'</ul>'
+    '<strong class="text-xl sm:text-2xl">{}</strong>'.format(key) + 
+    '<ul class="list-disc mb-5 sm:ml-5">' + 
+    ''.join(['<li><span class="text-sm sm:text-2xl">{}</span></li>'.format(suggestion) for suggestion in suggestions]) + 
+    '</ul>'
     for key, suggestions in possible_suggestions.items() if suggestions
   ])
  
