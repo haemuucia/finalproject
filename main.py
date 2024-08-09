4f75587e887014c611d5775ab24b7ffb99e8d858
haemuucia <162682171+haemuucia@users.noreply.github.com>
2024-08-09 21:31:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,7 @@ tailwind_cdn = """
 st.markdown(tailwind_cdn, unsafe_allow_html=True)
 
 # Load model
-model = joblib.load(os.path.join(os.path.dirname(__file__), "Model", "waterquality_model.pkl"))
-water = pd.read_csv('water_quality_data.csv')
+model = joblib.load("waterquality_model.pkl")
 
 # Predict Function
 def predict(data):
